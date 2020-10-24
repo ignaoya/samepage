df02cedd1d1ed313e844c09f97cb573a07d467f6
Ignacio Oyarzabal <ignaoya93@gmail.com>
2020-10-24 08:38:50 +0200

    Added .env.example file and passed mailgun API and DOmain from environment vars

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -1,10 +1,11 @@
+import os
 from typing import List
 from requests import Response, post
 
 
 class Mailgun:
-    MAILGUN_DOMAIN = ''
-    MAILGUN_API_KEY = ''
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
+    MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
 
     FROM_TITLE = 'Same Page'
     FROM_EMAIL = ''
